9306244a7ab69c5eb226f66834feb28faa023972
Magdalena <m.mandat@gmail.com>
2018-07-10 17:12:41 +0100

    owner added

---
+++ src/main/java/models/Owner.java
@@ -0,0 +1,54 @@
+package models;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Owner {
+    @Entity
+    @Table(name = "owners")
+    public class Owner {
+
+
+        private int id;
+        private String name;
+        private String userName;
+
+        public Owner() {
+        }
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        @Column(name = "name")
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        @Column(name = "userName")
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+    }
+
+
+}
