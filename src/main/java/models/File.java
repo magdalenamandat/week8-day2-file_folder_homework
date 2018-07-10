03c28cebbe90a0d6eaa4385d2885c801f93c7783
Magdalena <m.mandat@gmail.com>
2018-07-10 15:55:46 +0100

    hibernateUtil created

--- src/main/java/models/File.java
+++ src/main/java/models/File.java
@@ -1,4 +1,61 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class File {
+
+        private int id;
+        private String name;
+        private String extention;
+        private double size;
+
+        public File() {
+        }
+
+        public File(String name, String extention, double size) {
+            this.name = name;
+            this.extention = extention;
+            this.size = size;
+        }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "size")
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "extention")
+    public String getExtention() {
+        return extention;
+    }
+
+    public void setExtention(String extention) {
+        this.extention = extention;
+    }
 }
--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,4 +1,41 @@
 package models;
 
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+
+
+        private int id;
+        private String title;
+
+        public Folder() {
+        }
+
+
+        public Folder(String title) {
+            this.title = title;
+        }
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        @Column(name = "title")
+        public String getName() {
+            return title;
+        }
+
+        public void setName(String title) {
+            this.title = title;
+        }
+
+    }
 }
